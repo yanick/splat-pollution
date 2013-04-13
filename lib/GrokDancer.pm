57832a2b958e56a9f45a26ae8dfd7f3d73889413
Alois Mahdal <Alois.Mahdal@zxcvb.cz>
2013-04-13 16:45:59 +0200

    Added case with request->params() as suggested in #909
    *   https://github.com/PerlDancer/Dancer/issues/909#issuecomment-16296354

--- lib/GrokDancer.pm
+++ lib/GrokDancer.pm
@@ -4,10 +4,19 @@ use Dancer ':syntax';
 our $VERSION = '0.1';
 
 get qr{(.*)} => sub {
-    my $report = "Parameters received:\n";
+    my $report;
+
+    $report .= "Parameters received via `params()`:\n";
     foreach (keys %{&params}) {
         $report .= sprintf "*   %s => '%s'\n", $_, params->{$_};
     }
+
+    $report .= "Parameters received via `request->params('query')`:\n";
+    my $real_params = request->params('query');
+    foreach (keys %{$real_params}) {
+        $report .= sprintf "*   %s => '%s'\n", $_, params->{$_};
+    }
+
     return $report;
 };
 
